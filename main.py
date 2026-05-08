ce607182cba7394462dba7949c2dd89ab483639a
stepanyukvania-code <stepanyuk.vania@gmail.com>
2026-05-08 11:53:47 +0300

    fix main imports

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-
 import json
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from scanner.mercari import search
 from services.telegram import send
-from store import load_queries
+from data.store import load_queries
 
 SEEN_FILE = "data/seen.json"
 
